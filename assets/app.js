2327502920bcc90b23c7aad1d23fefc4bc00c28a
mehwish <mehwishgul715@gmail.com>
2022-08-30 19:55:20 +0500

    commit

--- assets/app.js
+++ assets/app.js
@@ -15,45 +15,45 @@ $(document).ready(function () {
 });
 
 
-// isotope
+isotope
 
-// $(document).ready(function () {
-//   let selector = $('.all').attr('data-filter');
-//   $('.reports .grid').isotope({
-//     filter: selector,
+$(document).ready(function () {
+  let selector = $('.all').attr('data-filter');
+  $('.reports .grid').isotope({
+    filter: selector,
 
-//   });
-//   return false;
+  });
+  return false;
 
-// });
-// $(document).ready(function () {
-//   let btn = $('.state__list');
+});
+$(document).ready(function () {
+  let btn = $('.state__list');
 
-//   btn.click(function (e) {
-//     $('.state__list button').removeClass("active-port");
-//     e.target.classList.add("active-port");
+  btn.click(function (e) {
+    $('.state__list button').removeClass("active-port");
+    e.target.classList.add("active-port");
 
-//     let selector = $(e.target).attr('data-filter');
-//     $('.reports .grid').isotope({
-//       filter: selector
-//     });
-//     return false;
-//   });
+    let selector = $(e.target).attr('data-filter');
+    $('.reports .grid').isotope({
+      filter: selector
+    });
+    return false;
+  });
 
-// });
+});
 // Dropzone library
 
-// var myDropzone = new Dropzone("#kt_dropzonejs_example_1", {
-//   url: "https://keenthemes.com/scripts/void.php", // Set the url for your upload script location
-//   paramName: "file", // The name that will be used to transfer the file
-//   maxFiles: 10,
-//   maxFilesize: 10, // MB
-//   addRemoveLinks: true,
-//   accept: function (file, done) {
-//     if (file.name == "wow.jpg") {
-//       done("Naha, you don't.");
-//     } else {
-//       done();
-//     }
-//   }
-// });
+var myDropzone = new Dropzone("#kt_dropzonejs_example_1", {
+  url: "https://keenthemes.com/scripts/void.php", // Set the url for your upload script location
+  paramName: "file", // The name that will be used to transfer the file
+  maxFiles: 10,
+  maxFilesize: 10, // MB
+  addRemoveLinks: true,
+  accept: function (file, done) {
+    if (file.name == "wow.jpg") {
+      done("Naha, you don't.");
+    } else {
+      done();
+    }
+  }
+});
